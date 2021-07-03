a3d33846143de22963d6f4976d7abcd38c3c52fa
atropos148 <tibor.h148@gmail.com>
2021-07-03 11:55:45 +0200

    Preparation for Favorites Route

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,21 @@
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
+
 import InputBase from '@material-ui/core/InputBase';
+
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
+  root: {
+    marginBottom: 5,
+  },
+
   searchBar: {
     backgroundColor: 'white',
-    borderRadius: '10px',
-    paddingLeft: '5px',
-    marginLeft: '5px',
+    borderRadius: 10,
+    paddingLeft: 5,
+    marginLeft: 5,
   },
 
   button: {
@@ -23,11 +29,13 @@ export default function NavBar() {
   const classes = useStyles();
 
   return (
-    <AppBar>
+    <AppBar className={classes.root}>
       <Toolbar>
         <Button className={classes.button}>Main Page</Button>
         <Button className={classes.button}>Favorites</Button>
-        <InputBase placeholder="Search Movie Name" className={classes.searchBar} />
+
+
+        <InputBase id='movieSearch' placeholder="Search Movie Name" className={classes.searchBar} />
       </Toolbar>
     </AppBar>
   )
--- src/pages/FavoritesPage.tsx
+++ src/pages/FavoritesPage.tsx
@@ -1,19 +1,16 @@
-import React from 'react';
-import './App.css';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
-// import Button from '@material-ui/core/Button';
+import Button from '@material-ui/core/Button';
 
-import NavBar from './components/NavBar';
-import MovieCard from './components/MovieCard';
+import NavBar from '../components/NavBar';
+import MovieCard from '../components/MovieCard';
 
-import { api_key } from './api_key';
-
-export default function App() {
+export default function FavoritesPage() {
   return (
     <Container maxWidth='sm'>
       <NavBar />
-      <Box my={10}>
+      <Box my={2}>
+        <Button color="primary">Hello World of Material-UI</Button>
         <MovieCard name="Tha Matrix" year={2000} />
         <MovieCard name="Akira" year={1987} />
       </Box>
